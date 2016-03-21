4be8d5f268aa369f577d41160cda6c380e28032a
gillesAzais <azaisgilles@gmail.com>
2016-03-21 15:59:50 +0100

    removed meteor

--- sudoku.js
+++ sudoku.js
@@ -1,4 +1,4 @@
-if (Meteor.isClient) {
+
 
     var Matrix9x9 = [ [ 0, 2, 0, 0, 0, 9, 0, 0, 6 ],
     [ 0, 1, 3, 0, 5, 4, 0, 0, 0 ],
@@ -30,6 +30,11 @@ var matrixWrongduplicatesQuadrant = [ [ 0, 2, 0, 0, 0, 9, 0, 0, 6 ],
     [ 0, 0, 0, 4, 3, 0, 7, 8, 0 ],
     [ 4, 0, 0, 9, 0, 0, 0, 1, 0 ] ];
 
+
+
+
+
+
 var getQuadrant = function(row,column,matrix){
 
     var arr = [];
@@ -40,6 +45,8 @@ var getQuadrant = function(row,column,matrix){
     }
     return arr;
 };
+
+
 
 var createSudoku = function(m) {
     matrixComplete=[];
@@ -75,19 +82,19 @@ var createSudoku = function(m) {
 
 function hasDuplicatesInSingleRows(matrix){
     var bool = false;
-    console.log(matrix, " isCorrect()");
+    //console.log(matrix, " isCorrect()");
     var row= -1;
-    console.log(matrix);
+    //console.log(matrix);
     matrix.some(function(array){
         row++;
-        console.log(array);
+        //console.log(array);
         for(var i = 0; i < array.length-1;i++){
             if(array[i] === 0){
                 continue;
             }
             for(var j = i+1; j<array.length;j++){
                 if(array[i] === array[j]){
-                    console.log("duplicates found in single row check");
+                    //console.log("duplicates found in single row check");
 
                     return  bool = true;
 
@@ -112,19 +119,19 @@ function hasDuplicateInQuadrant(matrix){
         }
         for(var j = i+1; j<arr.length;j++){
             if(arr[i] === arr[j]){
-                console.log( "DUPLICATES FOUND: ","columns:",i,"and",j );
+                //console.log( "DUPLICATES FOUND: ","columns:",i,"and",j );
                 return true;
                 return;
             }
         }
     }
-    console.log("no duplicates in quadrants check");
+    //console.log("no duplicates in quadrants check");
     return false;
 }
 
 
 function transpose(array){
-    console.log(array);
+    //console.log(array);
     var arrayLength = array[0].length;
     var newArray = [];
     for(var i = 0; i < array[0].length; i++){
@@ -141,13 +148,13 @@ function transpose(array){
 
 function isCorrect(sudoku){
     var bool = false;
-    console.log(sudoku.matrix, " isCorrect()");
+    //console.log(sudoku.matrix, " isCorrect()");
     if(!hasDuplicatesInSingleRows(sudoku.matrix) && !hasDuplicatesInSingleRows(transpose(sudoku.matrix))){
-
-        sudoku.quadrants().foreach(function(quadrant){
+    	
+        sudoku.quadrants().forEach(function(quadrant){
 
             if(!hasDuplicateInQuadrant(quadrant)){
-                console.log('sudoku is correct');
+                //console.log('sudoku is correct');
 
                return bool = true;
 
@@ -161,8 +168,8 @@ function isCorrect(sudoku){
 
     return bool;
 }
-//TODO: add Array to save configurations;
-function bruteForce(sudoku){
+
+function bruteForce(sudoku,history){
 //TODO: check comment
 /*
  check if correct, if yes   --> check zeros. If no zero present   --> complete;
@@ -170,106 +177,113 @@ function bruteForce(sudoku){
  check if correct, if no    --> go back to previous configuration and continue bruteforcing;
 
     */
+	
+	var bool = false;
+	var currentRecursiveHistory = history;
+	var uncheckedSudos = addNext(sudoku);
+	
+	uncheckedSudos.some(function(sudoku){
+		if(isCorrect(sudoku)){
+			currentRecursiveHistory.push(sudoku);
+	    	   if(isFull(sudoku)){
+	    	   //console.log(sudoku);
+	    	   bool = sudoku;
+	    	   return true;
+	    	   
+	       }else{
+	    	   console.log(sudoku);
+	    	   try{return bool = bruteForce(sudoku,currentRecursiveHistory);
+	    	   }catch(e){
+	    		   
+	    	   }
+	    	   
+	       }
+	        	
+	    }else{
+	    	console.log(sudoku);
+	    	sudoku = currentRecursiveHistory.pop();
+	    	 try{return bool = bruteForce(sudoku,currentRecursiveHistory);
+	    	   }catch(e){
+	    		   
+	    	   }
+	    	}
+	},this);
 
-    var newSudoku = addRandom(sudoku);
-
-    if(isDone(newSudoku)){
-        return sudoku;
-    }
-    if(isCorrect(newSudoku)){
-        console.log(newSudoku);
-        return bruteForce(newSudoku);
-    }
-    console.log("unsolvable");
-    return false;
+  return bool;
 
 
 }
 
-function addRandom(sudoku){
-    console.log(sudoku, "addrandom");
+function addNext(sudoku){
+    
+  uncheckedSudokus =[];
     var row= -1;
-    sudoku.matrix.forEach(function(array){
+    sudoku.matrix.some(function(array){
         row++;
         for(var i = 0; i < array.length-1;i++){
-            if(array[i] === 0){
-                array[i]= Math.floor(Math.random() * (10 - 1)) + 1;
-
-                break;
+            if(array[i] == 0){
+            	for(var i2 = 1; i2<10; i2++){
+            		var clone = jQuery.extend(true,{},sudoku);
+            		  array[i]=i2;
+            		  uncheckedSudokus.push(clone);
+            	}
+               
+               return true;
 
             }
         }
-       /* viewMatrix = sudoku.matrix;
-        Blaze.remove(template);
-        template = Blaze.render(Blaze.With(function () {
-            console.log(sudoku.matrix === Matrix9x9);
-
-            return sudoku.matrix;
-        },function(){return Template.sudo}),$('.sudoDiv')[0]);*/
+    
+     
     });
-    return sudoku;
+    
+    return uncheckedSudokus;
 }
 
-function isDone(sudoku){
+function isFull(sudoku){
 
     var bool = true;
     if(isCorrect(sudoku)){
         sudoku.matrix.every(function(array){
-            console.log(array);
+            //console.log(array);
             if(array.indexOf(0) > -1){
-                console.log('sudoku is incomplete, continue bruteforce');
+                //console.log('sudoku is incomplete, continue bruteforce');
                 return bool = false;
             }
         })
     }
-    if(bool){
 
-
-    viewMatrix = sudoku.matrix;
-    Blaze.remove(template);
-    template = Blaze.render(Blaze.With(function () {
-        console.log(sudoku.matrix === Matrix9x9);
-        return sudoku.matrix;
-    },function(){return Template.sudo}),$('.sudoDiv')[0]);
-
-    console.log('sudoku is complete, ending bruteforce');
-
-    }
     return bool;
 }
 
-
 viewMatrix = Matrix9x9;
 
-    Meteor.startup(function() {
-      template = Blaze.render(Blaze.With(function () {
+function renderMatrix(matrix){
+	var rowNr=0;
+	var columnNr=0;
+	$('<table class="sudoTable"></table>').append();
+	matrix.forEach(function(array){
+		columnNr=0;
+		rowNr++;
+			$('#tableDiv').append('<tr class="rowNr'+rowNr+'"></tr>');
+		array.forEach(function(e){
+			columnNr++
+			console.log("row: ",rowNr-1,"column: ",columnNr-1,columnNr,matrix[rowNr-1][columnNr-1]);
+			$('.rowNr'+rowNr).append('<td class="rowNr"'+rowNr+'>'+ matrix[rowNr-1][columnNr-1] +'</td>')
+			
+		
+ 		},this)
+	},this)
+}   
+ 
+    
+   
 
-            viewMatrix = Matrix9x9;
-          return viewMatrix;
-
-        },function(){return Template.sudo}),$('.sudoDiv')[0]);
-    });
-
-    Template.sudo.helpers({
-        sudoku: function () {
-
-            return viewMatrix;
-        }
-
-    });
-    Template.run.events(
-        {
-            'click #run': function (event) {
-                var matr = Matrix9x9;
-                bruteForce(createSudoku(matr));
-                Blaze.remove(template);
-                Blaze.render(Blaze.With(function () {
-                    return matr;
-                },function(){
-
-                    return Template.sudo}),$('.sudoDiv')[0]);
-
-
-            }
-        });
+onmessage=function(sudoku){
+	viewMatrix = sudoku.matrix;
+    Blaze.remove(template);
+    template = Blaze.render(Blaze.With(function () {
+        //console.log(sudoku.matrix === Matrix9x9);
+    	$('body').children()
+        return sudoku.matrix;
+    },function(){return Template.sudo}),$('.sudoDiv')[0]);
 }
